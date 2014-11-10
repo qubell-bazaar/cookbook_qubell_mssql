3ddd5b77bc5ebdc4610d5a061f10d95459dfc312
Anton Butovich <abutovich@gmail.com>
2014-11-10 15:38:05 +0200

    Recipies on database management added

--- Berksfile
+++ Berksfile
@@ -1,3 +1,5 @@
 source "https://api.berkshelf.com"
 
 metadata
+
+cookbook 'database', git: 'git@github.com:loky9000/database.git', branch: "fix_alter_role"
--- recipes/user_manage.rb
+++ recipes/user_manage.rb
@@ -14,6 +14,25 @@
     password node[:cookbook_qubell_mssql][:schema][:password]
     database_name node[:cookbook_qubell_mssql][:schema][:dbname]
     privileges node[:cookbook_qubell_mssql][:schema][:privileges]
+    sql_roles node[:cookbook_qubell_mssql][:schema][:db_roles]
     action node[:cookbook_qubell_mssql][:schema][:action]
+  end
+  sql_server_database_user node[:cookbook_qubell_mssql][:schema][:username] do
+    connection node[:cookbook_qubell_mssql][:db]
+    host host
+    password node[:cookbook_qubell_mssql][:schema][:password]
+    database_name node[:cookbook_qubell_mssql][:schema][:dbname]
+    privileges node[:cookbook_qubell_mssql][:schema][:privileges]
+    sql_roles node[:cookbook_qubell_mssql][:schema][:db_roles]
+    action :alter_roles
+  end
+  sql_server_database_user node[:cookbook_qubell_mssql][:schema][:username] do
+    connection node[:cookbook_qubell_mssql][:db]
+    host host
+    password node[:cookbook_qubell_mssql][:schema][:password]
+    database_name node[:cookbook_qubell_mssql][:schema][:dbname]
+    privileges node[:cookbook_qubell_mssql][:schema][:privileges]
+    sql_roles node[:cookbook_qubell_mssql][:schema][:db_roles]
+    action :grant
   end
 end
