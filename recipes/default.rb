71f3db7e6c522e5c4c278672f1a57e495f882acf
Artem Ivantsov <aivantsov@qubell.com>
2014-11-12 06:11:06 +0200

    update user_manage; .Net features

--- recipes/default.rb
+++ recipes/default.rb
@@ -13,6 +13,21 @@ else
   node.set['sql_server']['instance_name']="MSSQLSERVER"
 end
 
+windows_features=["NetFx4","NetFx3"]
+windows_features.each do |f|
+  windows_feature f do
+    action :install
+    all true
+  end
+end
+
+powershell_script "Install Web-Asp-Net45" do
+  flags "-ExecutionPolicy Unrestricted"
+  code <<-EOH
+    Add-WindowsFeature  Web-Asp-Net45
+  EOH
+end
+
 include_recipe "sql_server"
 include_recipe "sql_server::server"
 
@@ -21,5 +36,12 @@ directory "#{Chef::Config[:node_path]}" do
   action :create
   recursive true
   owner node['current_user']
+end
+
+powershell_script "disable_firewall" do
+  flags "-ExecutionPolicy Unrestricted"
+  code <<-EOH
+    netsh advfirewall set allprofiles state off
+  EOH
 end
 
--- recipes/user_manage.rb
+++ recipes/user_manage.rb
@@ -25,14 +25,6 @@
     privileges node[:cookbook_qubell_mssql][:schema][:privileges]
     sql_roles node[:cookbook_qubell_mssql][:schema][:db_roles]
     action :alter_roles
-  end
-  sql_server_database_user node[:cookbook_qubell_mssql][:schema][:username] do
-    connection node[:cookbook_qubell_mssql][:db]
-    host host
-    password node[:cookbook_qubell_mssql][:schema][:password]
-    database_name node[:cookbook_qubell_mssql][:schema][:dbname]
-    privileges node[:cookbook_qubell_mssql][:schema][:privileges]
-    sql_roles node[:cookbook_qubell_mssql][:schema][:db_roles]
-    action :grant
+    not_if { node[:cookbook_qubell_mssql][:schema][:action]=='drop' }
   end
 end
