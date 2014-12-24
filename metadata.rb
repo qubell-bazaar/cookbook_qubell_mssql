66963e33546f4dd5ca4533d9a5aeaf3bc9db21b8
Anton Butovich <abutovich@gmail.com>
2014-12-24 11:27:48 +0200

    Set metadata version to 0.1.1

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'abutovich@qubell.com'
 license          'All rights reserved'
 description      'Installs/Configures MSSQL server on Windows Server 2012R2'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
 
 depends          'sql_server', '>= 2.2.0'
 depends          'database', '>= 2.3.0'
